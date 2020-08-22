16bdd1c26781b9c3af3c59a16ab56b5a235cdcfe
Ed <64235029+Edacv2@users.noreply.github.com>
2020-08-22 18:37:02 -0400

    Add files via upload
    Simple captcha app

---
+++ code/Captcha.py
@@ -0,0 +1,31 @@
+import random
+from tkinter import *
+from tkinter import messagebox
+
+text = "abcdefghijklmnopqrstuvwxyz0123456789"
+window = Tk()
+window.title("Captcha App")
+window.geometry("300x100")
+captcha = StringVar()
+user_input = StringVar()
+
+def set_captcha():
+    s = random.choices(text, k = 6)
+    captcha.set(''.join(s))
+
+def check():
+    if captcha.get() == user_input.get():
+        messagebox.showinfo("Success", "Correct")
+    else:
+        messagebox.showinfo("Error", "Incorrect")
+    set_captcha()
+
+Label(window, textvariable = captcha,
+      font = "Arial 16 bold").pack()
+Entry(window, textvariable = user_input,
+      font = "Arial 10 bold").pack(ipady = 5)
+Button(window, command = check, text = "Check",
+       font = "Arial 10").pack()
+
+set_captcha()
+window.mainloop()
